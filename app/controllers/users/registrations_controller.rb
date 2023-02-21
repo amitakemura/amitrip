afa8490e55326100fd3567dda4d9891a190208b2
ami takemura <lovefuture217@outlook.jp>
2023-02-22 02:21:50 +0900

    masterKey生成

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -62,12 +62,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
 #アカウント登録後のリダイレクト先
 def after_sign_up_path_for(resource)
-  user_path(current_user.id)
+  hawaiians_path
 end
 
 #アカウント編集後のリダイレクト先
 def after_update_path_for(resource)
-  hawaiians_path
+  user_path(current_user.id)
 end
 
 end
